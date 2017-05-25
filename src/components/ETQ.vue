815c41176770d7ba7fdfd1e3dfab38a253f0717f
liudongge <liudongge@gomeholdings.com>
2017-05-25 21:17:47 +0800

    Change eslint configuration and add ETQ vue file

---
+++ src/components/ETQ.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <nav id="navBar" class="navBar fixed ready top-hide top" style="">
+            <button role="button" class="menu">
+                Menu<span class="label">MENU</span>
+            </button>
+            <span class="logo" data-href="https://www.etq-amsterdam.com">ETQ.</span>
+            <aside class="cartcount show">0<span class="label">BAG</span></aside>
+        </nav>
+        <figure id="overlay" class="overlay">
+            <button>X</button>
+        </figure>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ETQ',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* Inport css file from outsite */
+@import '../assets/css/app.min.css';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import ETQ from '@/components/ETQ'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'Hello',
+        component: Hello
+    }, {
+        path: '/ETQ',
+        name: 'ETQ',
+        component: ETQ
+    }]
 })
