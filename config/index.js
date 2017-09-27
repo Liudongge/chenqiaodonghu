1bb310717e49b9f87ca473d8cd664f79c114f604
Liudg <liudg2015@icloud.com>
2017-09-27 15:07:55 +0800

    change port to 6666

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 80,
+    port: 6666,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
